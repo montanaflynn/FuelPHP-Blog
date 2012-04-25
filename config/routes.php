c2239ace91fddf870ebb85cd3257a070a3caac7c
Montana Flynn <montana949@gmail.com>
2012-04-24 20:23:24 -0700

    Fixed admin routing

--- config/routes.php
+++ config/routes.php
@@ -1,6 +1,8 @@
 <?php
 return array(
 	'_root_'  => 'blog/index',
+	'admin'  => 'admin',
+	'admin/(:any)'  => 'admin/$1',
 	'(:any)'      => 'blog/post/$1', // Routes /blog/entry_name to /blog/entry/entry_name
 	'_404_'   => 'error/404',
 );
